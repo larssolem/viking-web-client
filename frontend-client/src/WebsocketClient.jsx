870cbb158461374a8104432c7629ad4a345ec1e6
larssolem <lars.solem@redpill-linpro.com>
2024-08-15 21:03:33 +0200

    change over too xterm.js

--- frontend-client/src/WebsocketClient.jsx
+++ frontend-client/src/WebsocketClient.jsx
@@ -1,7 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import React, {useState, useEffect, useRef} from 'react';
+import { Xterm } from 'xterm-react';
 
 const WebSocketClient = () => {
+    const [Terminal, setTerminal] = useState(null);
     const [messages, setMessages] = useState([]);
     const [inputMessage, setInputMessage] = useState('');
     const [isPaused, ] = useState(false);
@@ -27,10 +29,46 @@ const WebSocketClient = () => {
             console.log(e.data)
             console.log(JSON.stringify(e.data));
             const response = JSON.parse(e.data);
+            const text = response.Data.replace(/[^\x00-\x7F]/g, "");
             console.log(response)
-            setMessages([...messages, response.Data]);
+            setMessages([...messages, text]);
+
+            if (Terminal) {
+                Terminal.write(text);
+            }
         };
     }, [isPaused, messages]);
+
+    const onTermInit = (term) => {
+        if (!Terminal) {
+            console.log('init term', term);
+            setTerminal(term);
+            term.reset();
+            term.resize(100, 80)
+            messages.forEach(message => {
+                console.log("writing too term: ", message);
+                term.write(message);
+            })
+        }
+    }
+
+    const onTermDispose = (term) => {
+        setTerminal(null);
+    }
+
+    const handleData =(data) => {
+        if (Terminal) {
+            const code = data.charCodeAt(0)
+            if (code === 13 && inputMessage.length > 0) {
+                Terminal.write("\r\nEcho> " + inputMessage + "\r\n");
+            } else if (code < 32 || code === 127)  {
+                console.log('Controll key: ', code);
+            } else {
+                Terminal.write(data)
+                setInputMessage(inputMessage + data)
+            }
+        }
+    }
 
 
     const handleSendMessage = () => {
@@ -54,14 +92,17 @@ const WebSocketClient = () => {
 
     return (
         <div>
-            <div>
-                <h2>WebSocket Output:</h2>
-                <div>
-                    {messages.map((msg, index) => (
-                        <pre key={index}>{msg}</pre>
-                    ))}
-                </div>
-            </div>
+            { messages.length > 0 && (
+            <div className="App">
+                <header className="App-header">
+                    <Xterm
+                        onInit={onTermInit}
+                        onDispose={onTermDispose}
+                        onData={handleData}
+                        />
+                </header>
+            </div>)
+            }
             <div>
                 <input
                     type="text"
